936a028bfa168a517d37c5c55113770c32de6d1f
daw2-falchou21 <71894640+daw2-falchou21@users.noreply.github.com>
2020-10-09 17:48:35 +0200

    Create N2P1MaysaComments.php

---
+++ N2P1MaysaComments.php
@@ -0,0 +1,28 @@
+<?php
+echo "He aprendido hacer un GET Y un POST";
+echo '<br>';
+echo "He aprendido hacer unas COOKIE Y SETCOOKIE";
+echo '<br>';
+echo "Ha crear una urlencode y que este conectada con la otra pagina";
+echo '<br>';
+echo "He aprendido hacer formularios con la etiqueta form input de html";
+echo '<br>';
+echo "He aprendido a saber que php trabaja con html";
+echo '<br>';
+echo "He aprendido a crear arrays nuevas y conectarlas entre ellas";
+echo '<br>';
+echo "He visto todos los arrays que ni conocia y he conectado un enlace con la otra pagina por primera vez";
+echo '<br>';
+echo "He hecho el null que al final me a salido y a sido mas facil de lo que pensaba";
+echo '<br>';
+echo "He hecho el date que ha sido muy facil";
+echo '<br>';
+echo "El date te da diferentes opciones para poder poner la fecha";
+echo '<br>';
+echo "El documento le doy un 6 porque al principio no entendia nada de nada pero luego ya al final sabia lo que queria";
+echo '<br>';
+echo "La calificacion del professor le doy un 5 pero la explicacion de ayer le doy un 7";
+echo '<br>';
+echo "Que podrias explicarlo como hicistes la explicacion del jueves dibujandolo con las etiquetas donde deberia estar cada pagina";
+echo '<br>';
+?>
